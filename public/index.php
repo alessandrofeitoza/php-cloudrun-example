3f3f263b46854f89edd1dfe2dadbcf203ca1c414
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-12-09 21:37:16 -0300

    alterando index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
 
-echo "alo som";
-
-echo "<hr>";
-
-echo "Dev Fest 2025";
-
-echo "como dizia minha ex: terminamos";
-
+echo "<h1>Imersão Fullcycle: 09/12/2025</h1>";
 // https://php-devfest-1002804070054.europe-west1.run.app/
